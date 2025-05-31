a06cb675bf99544142b9766008324870dfe8f482
Anurag Kumar Singh <anuragkr@Anurags-Mac-mini.local>
2025-05-31 16:53:36 +0530

    made a code workable

---
+++ config.py
@@ -0,0 +1,9 @@
+# config.py
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+TAVILY_API_KEY = os.getenv("TAVILY_API_KEY")
+HF_TOKEN = os.getenv("HF_TOKEN")
